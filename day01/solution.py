9162c3f82c83f21f4e426e3dac1e2f38434b9f85
Igor Vaiman <igor.vaiman@inten.to>
2023-12-06 15:04:36 +0100

    day 1 optimized

--- day01/solution.py
+++ day01/solution.py
@@ -4,30 +4,30 @@ from typing import Callable
 
 def extract_digits(
     inp: str,
-    digit_re: re.Pattern,
+    digit_patterns: list[str],
     digit_parser: Callable[[str], int],
     debug: bool,
 ) -> list[int]:
+    fwd_digit_re = re.compile("|".join(re.escape(p) for p in digit_patterns))
+    bck_digit_re = re.compile("|".join(re.escape(p[::-1]) for p in digit_patterns))
     first_last_digits: list[int] = []
     for line in inp.splitlines():
-        digits_matched: list[str] = []
-        for pos in range(len(line)):
-            m = digit_re.match(line, pos)
-            if m:
-                digits_matched.append(m.group())
-        if not digits_matched:
-            raise ValueError(f"failed to parse line: {line}")
-        digits = [digit_parser(ds) for ds in digits_matched]
-        first_last_digit_num = int(str(digits[0]) + str(digits[-1]))
+        first_digit_match = fwd_digit_re.search(line)
+        assert first_digit_match
+        last_digit_match = bck_digit_re.search(line[::-1])
+        assert last_digit_match
+        first_last_digit_num = digit_parser(first_digit_match.group()) * 10 + digit_parser(
+            last_digit_match.group()[::-1]
+        )
         if debug:
-            print(f"{line} -> {digits_matched} -> {digits} -> {first_last_digit_num}")
+            print(f"{line} -> {first_last_digit_num}")
         first_last_digits.append(first_last_digit_num)
     return first_last_digits
 
 
 def part_1(inp: str, debug: bool):
     calibration_values = extract_digits(
-        inp, digit_re=re.compile(r"\d"), digit_parser=int, debug=debug
+        inp=inp, digit_patterns=[str(i) for i in range(10)], digit_parser=int, debug=debug
     )
     if debug:
         print(calibration_values)
@@ -49,7 +49,7 @@ def part_2(inp: str, debug: bool):
     digit_spelling_to_value = {spelling: idx + 1 for idx, spelling in enumerate(digit_spelling)}
     calibration_values = extract_digits(
         inp,
-        digit_re=re.compile("(" + "|".join([r"\d"] + digit_spelling) + ")"),
+        digit_patterns=[str(i) for i in range(10)] + digit_spelling,
         digit_parser=lambda digit: digit_spelling_to_value.get(digit) or int(digit),
         debug=debug,
     )
