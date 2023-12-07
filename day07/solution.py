bcd31f85de0822b7cf8b3e06dac1917fa742edc3
Igor Vaiman <igor.vaiman@inten.to>
2023-12-07 12:21:20 +0100

    day7 profiling and minor optimizations

--- day07/solution.py
+++ day07/solution.py
@@ -4,14 +4,78 @@ from dataclasses import dataclass
 from typing import ClassVar, Literal
 
 
+def card_rank_pt1(card: str) -> int:
+    match card:
+        case "2":
+            return 0
+        case "3":
+            return 1
+        case "4":
+            return 2
+        case "5":
+            return 3
+        case "6":
+            return 4
+        case "7":
+            return 5
+        case "6":
+            return 6
+        case "8":
+            return 7
+        case "9":
+            return 8
+        case "T":
+            return 9
+        case "J":
+            return 10
+        case "Q":
+            return 11
+        case "K":
+            return 12
+        case "A":
+            return 13
+    raise ValueError(f"Unexpected card: {card}")
+
+
+def card_rank_pt2(card: str) -> int:
+    match card:
+        case "J":
+            return 0
+        case "2":
+            return 1
+        case "3":
+            return 2
+        case "4":
+            return 3
+        case "5":
+            return 4
+        case "6":
+            return 5
+        case "7":
+            return 6
+        case "6":
+            return 7
+        case "8":
+            return 8
+        case "9":
+            return 9
+        case "T":
+            return 10
+        case "Q":
+            return 11
+        case "K":
+            return 12
+        case "A":
+            return 13
+    raise ValueError(f"Unexpected card: {card}")
+
+
 @functools.total_ordering
 @dataclass
 class Hand:
     cards: list[str]
     part: Literal[1, 2]
 
-    CARDS_RANKING_PT1: ClassVar = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
-    CARDS_RANKING_PT2: ClassVar = ["J", "2", "3", "4", "5", "6", "7", "8", "9", "T", "Q", "K", "A"]
     HAND_TYPES_RANKING: ClassVar = [
         [5],  # five of a kind
         [4, 1],  # four of a kind
@@ -23,12 +87,12 @@ class Hand:
     ]
 
     def __post_init__(self) -> None:
-        RANKING = self.CARDS_RANKING_PT1 if self.part == 1 else self.CARDS_RANKING_PT2
-        self.card_ranks = [RANKING.index(card) for card in self.cards]
-        card_count = collections.Counter(self.cards)
-        if self.part == 2 and "J" in card_count and len(card_count) > 1:
+        card_rank_fn = card_rank_pt1 if self.part == 1 else card_rank_pt2
+        self.card_ranks = [card_rank_fn(card) for card in self.cards]
+        card_count = collections.Counter(self.card_ranks)
+        if self.part == 2 and 0 in card_count and len(card_count) > 1:
             # J is a Joker and it pretends to be the most counted card to increase hand rank
-            joker_count = card_count.pop("J")
+            joker_count = card_count.pop(0)
             max_count_card = max(card_count.items(), key=lambda cc: cc[1])[0]
             card_count[max_count_card] += joker_count
         self.type = sorted(card_count.values(), reverse=True)
