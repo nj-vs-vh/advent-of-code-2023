5b9df5fed923fcb07eac9d535a48eb5d2c8f600e
Igor Vaiman <igor.vaiman@inten.to>
2023-12-21 12:15:07 +0100

    d20

--- day20/solution.py
+++ day20/solution.py
@@ -1,6 +1,7 @@
 import collections
 import itertools
 import json
+import math
 import re
 from dataclasses import asdict, dataclass
 from pydoc import ModuleScanner
@@ -35,10 +36,21 @@ class Module:
             name=m.group(2),
             type_=type_,
             targets=[t.strip() for t in m.group(3).split(",")],
-            origins=[],  # filled later
+            origins=[],  # filled later in parse_system
         )
 
+    @classmethod
+    def parse_system(cls, inp: str) -> "ModuleSystem":
+        modules_list = [Module.parse(line) for line in inp.splitlines()]
+        modules = {m.name: m for m in modules_list}
+        for origin in modules_list:
+            for target_name in origin.targets:
+                if module := modules.get(target_name):
+                    module.origins.append(origin.name)
+        return modules
 
+
+ModuleSystem = dict[str, Module]
 Signal = bool
 LOW: Signal = False
 HIGH: Signal = True
@@ -53,31 +65,31 @@ class ModulesState:
         return hash(json.dumps(asdict(self), check_circular=False, sort_keys=True))
 
     @classmethod
-    def initial(cls, modules: list[Module]) -> "ModulesState":
+    def initial(cls, system: ModuleSystem) -> "ModulesState":
         return ModulesState(
-            flip_flops={m.name: LOW for m in modules if m.type_ == "flip-flop"},
+            flip_flops={m.name: LOW for m in system.values() if m.type_ == "flip-flop"},
             conjuctions={
                 m.name: {origin: LOW for origin in m.origins}
-                for m in modules
+                for m in system.values()
                 if m.type_ == "conjunction"
             },
         )
 
 
-def push_button(modules: dict[str, Module], state: ModulesState) -> dict[str, list[Signal]]:
+def push_button(system: ModuleSystem, state: ModulesState) -> dict[str, list[Signal]]:
     signals_received: dict[str, list[Signal]] = collections.defaultdict(list)
     signals_received[BROADCASTER] = [LOW]
 
     signal_queue = collections.deque[tuple[str, str, Signal]](
-        (BROADCASTER, target, LOW) for target in modules[BROADCASTER].targets
+        (BROADCASTER, target, LOW) for target in system[BROADCASTER].targets
     )
     while signal_queue:
         origin_name, target_name, signal = signal_queue.popleft()
         signals_received[target_name].append(signal)
-        target = modules.get(target_name)
+        target = system.get(target_name)
         if target is None:
             continue
-        origin = modules[origin_name]
+        origin = system[origin_name]
         if target.type_ == "flip-flop" and signal is LOW:
             flipped = not state.flip_flops[target.name]
             state.flip_flops[target.name] = flipped
@@ -90,35 +102,63 @@ def push_button(modules: dict[str, Module], state: ModulesState) -> dict[str, li
 
 
 def part_1(inp: str, debug: bool):
-    modules = [Module.parse(line) for line in inp.splitlines()]
-    modules_by_name = {m.name: m for m in modules}
+    system = Module.parse_system(inp)
     if debug:
-        print(*modules, sep="\n")
-        print(modules_by_name, sep="\n")
-    for origin in modules:
-        for target_name in origin.targets:
-            if module := modules_by_name.get(target_name):
-                module.origins.append(origin.name)
+        print(*system.values(), sep="\n")
 
-    state = ModulesState.initial(modules)
+    state = ModulesState.initial(system)
     signal_counter: dict[Signal, int] = {LOW: 0, HIGH: 0}
     for _ in range(1000):
-        for signal in itertools.chain.from_iterable(push_button(modules_by_name, state).values()):
+        for signal in itertools.chain.from_iterable(push_button(system, state).values()):
             signal_counter[signal] += 1
     print(signal_counter[LOW] * signal_counter[HIGH])
 
 
 def part_2(inp: str, debug: bool):
-    modules = [Module.parse(line) for line in inp.splitlines()]
-    modules_by_name = {m.name: m for m in modules}
-    for origin in modules:
-        for target_name in origin.targets:
-            if module := modules_by_name.get(target_name):
-                module.origins.append(origin.name)
+    system = Module.parse_system(inp)
+    rx_origins = [m for m in system.values() if "rx" in m.targets]
 
-    state = ModulesState.initial(modules)
-    for button_press in itertools.count():
-        signals_received = push_button(modules_by_name, state)
-        if LOW in signals_received["rx"]:
-            print(button_press)
-            break
+    # the input has very specific properties, check them here
+    assert len(rx_origins) == 1
+    rx_origin = rx_origins[0]
+    # rx parent must be a NAND gate with multiple inputs
+    assert rx_origin.type_ == "conjunction"
+    subsystems: list[tuple[str, ModuleSystem]] = []
+    for subsystem_target in rx_origin.origins:
+        # rx grandparents must be NAND gates with 1 input = simple NOT gates
+        assert system[subsystem_target].type_ == "conjunction"
+        assert len(system[subsystem_target].origins) == 1
+        subsystem: ModuleSystem = dict()
+        queue = collections.deque[str]([subsystem_target])
+        while queue:
+            prev_origin = queue.popleft()
+            if prev_origin in subsystem:
+                continue
+            subsystem[prev_origin] = system[prev_origin]
+            queue.extend(system[prev_origin].origins)
+        subsystems.append((subsystem_target, subsystem))
+
+    # check that subsystems don't overlap (except for broadcaster), so we can simulate them independently
+    for _, subsystem_1 in subsystems:
+        for _, subsystem_2 in subsystems:
+            if subsystem_1 is subsystem_2:
+                continue
+            assert set(subsystem_1.keys()).intersection(subsystem_2.keys()) == {BROADCASTER}
+
+    # finding a cycle for each subsection
+    periods: list[int] = []
+    for target, subsystem in subsystems:
+        state = ModulesState.initial(subsystem)
+        rx_triggering_steps: list[int] = []
+        for step in itertools.count():
+            received_signals = push_button(subsystem, state)
+            if LOW in received_signals[target]:
+                rx_triggering_steps.append(step)
+                if len(rx_triggering_steps) > 2:
+                    break
+        period = rx_triggering_steps[1] - rx_triggering_steps[0]
+        # same condition for LCM applicability as in day 8
+        assert period == rx_triggering_steps[0] + 1
+        periods.append(period)
+
+    print(math.lcm(*periods))
